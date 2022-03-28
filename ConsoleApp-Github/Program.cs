77a423e88143a9aad42be579ca3f129e02cdbb29
Anshurajgupta01 <102514170+Anshurajgupta01@users.noreply.github.com>
2022-03-28 17:01:31 +0530

    Revert "Changing From another account (fork)"

--- ConsoleApp-Github/Program.cs
+++ ConsoleApp-Github/Program.cs
@@ -12,7 +12,6 @@ namespace ConsoleApp_Github
         {
             Console.WriteLine(" Branch 1 .....");
             Console.WriteLine("New Branch...");
-            Console.WriteLine(" Changing my original repo using Fork .....");
         }
     }
 }
